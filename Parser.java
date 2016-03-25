22e716dda016d4be9c54204fe70a82276101cfda
Jude Tillekeratne <jtilleke@uwaterloo.ca>
2016-03-25 15:24:02 -0400

    Fixed the parse function Key: Function, Value = list of scopes the function belongs to

--- Parser.java
+++ Parser.java
@@ -10,80 +10,78 @@ import java.util.regex.Pattern;
  */
 public class Parser {
 
-	private HashMap<Integer, HashSet<String>> FOccurrences;
-	private ArrayList<String> functions;
+    public void parse(File file) {
+        FOccurrences = new HashMap<Integer,HashSet<String>>();
+        functions = new ArrayList<String>();
+        try {
+            BufferedReader lines = new BufferedReader(new FileReader(file));
+            String line;
+            String sKey = null;
 
-	public HashMap<Integer, HashSet<String>> getFOccurrences() {
-		return FOccurrences;
-	}
+            while ((line = lines.readLine()) != null) {
+                if (line.contains("null function")) {
+                    //currently adding functions
+                    sKey = "functions";
+                }
+                if (line.contains("Call graph node")) {
+                    //skey is the currentscope
+                    Pattern pattern = Pattern.compile("'(.*?)'");
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        sKey = (matcher.group(0));
+                        sKey = sKey.replace("'","");
+                    }
+                } else if (line.contains("calls function")) {
+                    String functionName;
+                    if (sKey.equals("functions")) {
+                        String[] Tokens = line.split("\\s+");
+                        functionName = Tokens[Tokens.length - 1];
+                        functionName = functionName.replace("'", "");
+                        functions.add(functionName);
+                    }
+                    else
+                    {
+                        // get the function value
+                        String[] temp = line.split(" ");
+                        //get the last token
+                        functionName = temp[temp.length - 1];
+                        //remove the quotes
+                        functionName = functionName.replace("'", "");
+                    }
 
-	public ArrayList<String> getFunctions() {
-		return functions;
-	}
+                    if (FOccurrences.containsKey(functionName.hashCode()))
+                    {
+                        //add the current scope to the functionName
+                        FOccurrences.get(functionName.hashCode()).add(sKey);
+                    }
+                    else
+                    {
+                        FOccurrences.put(functionName.hashCode(), new HashSet<String>());
+                    }
+                }
+            }
+            for (int i = 0 ; i < functions.size();i++)
+            {
+                //System.out.println(functions.get(i));
+            }
+            for (Integer key: FOccurrences.keySet())
+            {
+                //System.out.println(key + " : "+ FOccurrences.get(key));
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        //Change the filename to any textfile location to test the parser
+        String directory = System.getProperty("user.dir");
+        String fileName = directory + "\\main.txt";
+        File file = new File(fileName);
+        Parser parser= new Parser();
+        parser.parse(file);
+    }
 
-	public void parse(File file) {
-		FOccurrences = new HashMap<Integer, HashSet<String>>();
-		functions = new ArrayList<String>();
-		
-		try {
-			BufferedReader lines = new BufferedReader(new FileReader(file));
-			String line;
-			String sKey = null;
-			HashSet<String> tempHash = new HashSet<String>();
-			// temporary hashset for displaying strings for sets, delete it
-			// after testing
-			HashSet<String> tempHashString = new HashSet<String>();
-
-			while ((line = lines.readLine()) != null) {
-				if (line.contains("null function")) {
-					// currently adding functions
-					sKey = "functions";
-				}
-				if (line.contains("Call graph node ")) {
-					// if its not null and not adding functions
-					if (sKey != null && !sKey.equals("functions")) {
-						// add to hashmap
-						FOccurrences.put(sKey.hashCode(), tempHash);
-						System.out.println(sKey + " : " + tempHashString);
-					}
-					Pattern pattern = Pattern.compile("'(.*?)'");
-					Matcher matcher = pattern.matcher(line);
-					if (matcher.find()) {
-						sKey = (matcher.group(0));
-						sKey = sKey.replace("'", "");
-					}
-					// reset the temp hashset;
-					tempHash = new HashSet<String>();
-					// reset the testing object
-					tempHashString = new HashSet<String>();
-				} else if (line.contains("calls function")) {
-					if (sKey.equals("functions")) {
-						String[] Tokens = line.split("\\s+");
-						String functionName = Tokens[Tokens.length - 1];
-						functionName = functionName.replace("'", "");
-						functions.add(functionName);
-						if (!FOccurrences.containsKey(functionName)){
-							FOccurrences.put(functionName.hashCode(), new HashSet<String>());
-						}
-						System.out.println(functionName);
-					} else {
-						// get the function value
-						String[] temp = line.split(" ");
-						// get the last token
-						String functionName = temp[temp.length - 1];
-						// remove the quotes
-						functionName = functionName.replace("'", "");
-						tempHash.add(functionName);
-						// add to to the testing object
-						tempHashString.add(functionName);
-					}
-				}
-
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 }
