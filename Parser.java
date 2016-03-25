9f44606d795efd3f16c82e35a2ecdf59975a5f87
Jude Tillekeratne <jtilleke@uwaterloo.ca>
2016-03-25 14:02:02 -0400

    Added functions to functions ArrayList and fixed quotes

--- Parser.java
+++ Parser.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.regex.Matcher;
@@ -10,45 +11,57 @@ import java.util.regex.Pattern;
 public class Parser {
 
     public void parse(File file) {
-        HashMap<Integer, HashSet<Integer>> FOccurrences = new HashMap<Integer,HashSet<Integer>>();
+        HashMap<Integer, HashSet<String>> FOccurrences = new HashMap<Integer,HashSet<String>>();
+        ArrayList<String> functions = new ArrayList<String>();
         try {
             BufferedReader lines = new BufferedReader(new FileReader(file));
             String line;
             String sKey = null;
-            HashSet<Integer> tempHash = new HashSet<Integer>();
+            HashSet<String> tempHash = new HashSet<String>();
             //temporary hashset for displaying strings for sets, delete it after testing
             HashSet<String> tempHashString = new HashSet<String>();
 
             while ((line = lines.readLine()) != null) {
                 if (line.contains("null function")) {
-                    //ignore the first calls function
-                    sKey = null;
+                    //currently adding functions
+                    sKey = "functions";
                 }
                 if (line.contains("Call graph node ")) {
-                    if (sKey != null) {
+                    //if its not null and not adding functions
+                    if (sKey != null && !sKey.equals("functions")) {
                         //add to hashmap
                         FOccurrences.put(sKey.hashCode(), tempHash);
-                        System.out.println(sKey + " : " + tempHashString);
+                        //System.out.println(sKey + " : " + tempHashString);
                     }
                     Pattern pattern = Pattern.compile("'(.*?)'");
                     Matcher matcher = pattern.matcher(line);
                     if (matcher.find()) {
                         sKey = (matcher.group(0));
+                        sKey = sKey.replace("'","");
                     }
                     //reset the temp hashset;
-                    tempHash = new HashSet<Integer>();
+                    tempHash = new HashSet<String>();
                     //reset the testing object
                     tempHashString = new HashSet<String>();
                 } else if (line.contains("calls function")) {
-                    // get the function value
-                    String[] temp = line.split(" ");
-                    //get the last token
-                    String functionName = temp[temp.length - 1];
-                    //remove the quotes
-                    functionName = functionName.replace("'", "");
-                    tempHash.add(functionName.hashCode());
-                    //add to to the testing object
-                    tempHashString.add(functionName);
+                    if (sKey.equals("functions")) {
+                        String[] Tokens = line.split("\\s+");
+                        String functionName = Tokens[Tokens.length - 1];
+                        functionName = functionName.replace("'", "");
+                        //System.out.println(functionName);
+                    }
+                    else
+                    {
+                        // get the function value
+                        String[] temp = line.split(" ");
+                        //get the last token
+                        String functionName = temp[temp.length - 1];
+                        //remove the quotes
+                        functionName = functionName.replace("'", "");
+                        tempHash.add(functionName);
+                        //add to to the testing object
+                        tempHashString.add(functionName);
+                    }
                 }
 
 
